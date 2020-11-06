b8199c1c29a3adc3eadf4bd9f6c8968b4b3f6ccf
tareq hajjer <tareqhajer@gmail.com>
2020-11-06 07:39:44 +0200

    Add Migration Drop Foregin key

---
+++ BWR.Infrastructure/Migrations/202011060529057_drop FK_dbo.MoneyAction_dbo.Clearing_TransactionId constraint.cs
@@ -0,0 +1,17 @@
+﻿namespace BWR.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dropFK_dboMoneyAction_dboClearing_TransactionIdconstraint : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.MoneyAction", "FK_dbo.MoneyAction_dbo.Clearing_TransactionId");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
